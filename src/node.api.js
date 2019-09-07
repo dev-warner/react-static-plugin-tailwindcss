4d26396ca7619bc40683c3f1ebfa3c511501b53d
Joe Warner <fed.warner@gmail.com>
2019-09-07 21:22:55 +0100

    fix(babel & formatting) added env preset and formatted

--- src/node.api.js
+++ src/node.api.js
@@ -1,40 +1,37 @@
-const POSTCSS_LOADER = 'postcss-loader';
+const POSTCSS_LOADER = "postcss-loader";
 const isPostCssLoader = ({ loader } = {}) => loader === POSTCSS_LOADER;
 const getOneOf = ({ oneOf } = {}) => {
-    if (oneOf) {
-        return oneOf;
-    }
+  if (oneOf) {
+    return oneOf;
+  }
 
-    return [];
+  return [];
 };
 
 export default _ => ({
-    webpack: config => {
-        const { rules } = config.module;
-        const { oneOf } = rules.find(getOneOf);
+  webpack: config => {
+    const { rules } = config.module;
+    const { oneOf } = rules.find(getOneOf);
 
-        for (const item of oneOf) {
-            const postcssLoader = (
-                item &&
-                item.loader &&
-                item.loader.find(isPostCssLoader)
-            );
+    for (const item of oneOf) {
+      const postcssLoader =
+        item && item.loader && item.loader.find(isPostCssLoader);
 
-            if (postcssLoader && postcssLoader.options && postcssLoader.options.plugins) {
+      if (
+        postcssLoader &&
+        postcssLoader.options &&
+        postcssLoader.options.plugins
+      ) {
+        const plugins = postcssLoader.options.plugins;
 
-                const plugins = postcssLoader.options.plugins;
+        Object.assign(postcssLoader.options, {
+          plugins: [require("tailwindcss"), ...(plugins.length ? plugins : [])]
+        });
 
-                Object.assign(postcssLoader.options, {
-                    plugins: [
-                        require('tailwindcss'),
-                        ...(plugins.length ? plugins : [])
-                    ]
-                })
-
-                break;
-            }
-        }
-
-        return config
+        break;
+      }
     }
-})
+
+    return config;
+  }
+});
